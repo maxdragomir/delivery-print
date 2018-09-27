dc85c067f2e5f537685869fafd14636031cda7ba
maxim1994 <rbcnjxrf146>
2018-09-27 20:35:38 +0300

    complete all work

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,15 +1,20 @@
 $(document).ready(function () {
-    let btn = $('.button');
+    let btn = $('.button'),
+        title = $('.blank-title');
 
     setTimeout(function () {
         btn.addClass('show');
     }, 1500);
 
     btn.on('click', function () {
-
         $(this).toggleClass('active');
         $('body').toggleClass('hide');
         $('.checkbox').toggleClass('checked');
-
     });
+
+    title.on('click', function () {
+       $(this).next().slideToggle();
+       $(this).children('.title-number').toggleClass('animate');
+    });
+
 });
